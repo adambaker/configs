7676cc39333fa1a763c370f93d050f8e7fbf6a41
adam <adam@tychon.(none)>
2012-06-07 02:35:37 -0500

    oh-my-zsh installer.
    wgets oh-my-zsh, installs it, then symlinks the zshrc into
    ~/.zshrc

--- install/ohmysh.sh
+++ install/ohmysh.sh
@@ -4,5 +4,18 @@ if [ -z `which zsh` ]; then
   exit 1;
 fi
 
-#install_path=$(dirname "$PWD/$0")
-#echo $install_path
+old_dir=$PWD
+install_path=$(dirname "$PWD/$0")
+cd
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+if [ -e .zshrc ]; then
+  if [ -f .zshrc ]; then 
+    echo 'Backing up .zshrc as .zshrc.old';
+    mv .zshrc .zshrc.old
+  else
+    echo '.zshrc is not a normal file. Remove it or back it up and re-run'
+    exit 1;
+  fi
+  ln -s "$install_path/../zshrc" .zshrc
+fi
+cd $old_dir
