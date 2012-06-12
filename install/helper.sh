7598f300834fc76aa2cf9668ec3177ce784c6a0f
Adam C. Baker <adam.durandal@gmail.com>
2012-06-12 02:21:52 -0500

    create helper functions for installers.

---
+++ install/helper.sh
@@ -0,0 +1,18 @@
+function link_config {
+  fn=$1; install_path=$2;
+  if [ -e ".$fn" ]; then
+    if [ -h ".$fn" ]; then
+      echo "Deleting .$fn link.";
+      rm ".$fn"
+    else
+      if [ -f .zshrc ]; then 
+        echo "Backing up .$fn as .$fn.old";
+        mv ".$fn" ".$fn.old"
+      else
+        echo ".$fn is not a normal file. Remove it or back it up and re-run"
+        exit 1;
+      fi
+    fi
+  fi
+  ln -s "$install_path/../$fn" ".$fn"
+}
--- install/janus.sh
+++ install/janus.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-for prereq in curl rake
+install_path=$(dirname "$PWD/$0")
+source "$install_path/helper.sh";
+
+for prereq in vomit curl rake
 do
   if [ -z `which $prereq` ]; then 
     echo "You must install $prereq first."
@@ -13,21 +16,7 @@ curl -Lo- https://bit.ly/janus-bootstrap | bash
 echo 'symlinking janus customizations';
 for fn in vimrc.after vimrc.before
 do
-  if [ -e ".$fn" ]; then
-    if [ -h ".$fn" ]; then
-      echo "Deleting .$fn link.";
-      rm ".$fn"
-    else
-      if [ -f .zshrc ]; then 
-        echo "Backing up .$fn as .$fn.old";
-        mv ".$fn" ".$fn.old"
-      else
-        echo ".$fn is not a normal file. Remove it or back it up and re-run"
-        exit 1;
-      fi
-    fi
-  fi
-  ln -s "$install_path/../$fn" ".$fn"
+  link_config $fn $install_path
 done
  
 echo 'adding bufkill plugin'
