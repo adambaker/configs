dfdc312fc603326423c627b59e92d96863068d58
Adam C. Baker <adam.durandal@gmail.com>
2015-04-17 13:52:29 -0700

    load gnome-terminal settings

--- install/debian.sh
+++ install/debian.sh
@@ -1,1 +1,3 @@
-sudo apt-get install zsh gtk-redshift vim tmux rxvt-unicode curl exuberant-ctags postgresql pgadmin3 libdbd-pg-perl build-essential cpanminus perl perl-doc
+sudo apt-get install zsh gtk-redshift tree vim tmux rxvt-unicode curl exuberant-ctags postgresql pgadmin3 libdbd-pg-perl build-essential cpanminus perl perl-doc
+install_path=$(dirname "$PWD/$0")
+gconftool-2 --load "$install_path/../gnome-terminal-conf.xml"
