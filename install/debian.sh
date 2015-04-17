2c153bffcb2b3006b5ed7bb1a75cdb4bff55c41d
Adam C. Baker <adam.durandal@gmail.com>
2015-04-16 22:52:29 -0700

    Update debian.sh

--- install/debian.sh
+++ install/debian.sh
@@ -1,1 +1,1 @@
-sudo apt-get install zsh vim tmux rxvt-unicode curl exuberant-ctags postgresql pgadmin3 libdbd-pg-perl build-essential cpanminus perl perl-doc
+sudo apt-get install zsh gtk-redshift vim tmux rxvt-unicode curl exuberant-ctags postgresql pgadmin3 libdbd-pg-perl build-essential cpanminus perl perl-doc
