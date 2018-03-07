e146305319a3e89eea63524a6537fe8235890470
Adam C. Baker <adam.durandal@gmail.com>
2018-03-07 15:55:26 -0800

    delete submodule, kill nodejs install and gnome-term

--- install/debian.sh
+++ install/debian.sh
@@ -1,7 +1,3 @@
 sudo apt-get update
 sudo apt-get install zsh gtk-redshift tree vim tmux rxvt-unicode curl libcurl4-openssl-dev exuberant-ctags postgresql pgadmin3 libpq-dev libdbd-pg-perl build-essential cpanminus perl perl-doc software-properties-common
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install nodejs
 
-install_path=$(dirname "$PWD/$0")
-gconftool-2 --load "$install_path/../gnome-terminal-conf.xml"
