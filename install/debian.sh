880030f159967a80edd835b724c7ed2703fe53d2
Adam C. Baker <adam.baker@workday.com>
2018-03-12 22:31:51 -0700

    how have I forgotten ag for so long?

--- install/debian.sh
+++ install/debian.sh
@@ -1,4 +1,4 @@
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 
-sudo apt-get install zsh gtk-redshift tree neovim tmux rxvt-unicode curl libcurl4-openssl-dev exuberant-ctags postgresql pgadmin3 libpq-dev libdbd-pg-perl build-essential cpanminus perl perl-doc software-properties-common python-software-properties python-dev python-pip python3-dev python3-pip
+sudo apt-get install zsh gtk-redshift tree neovim tmux silversearcher-ag rxvt-unicode curl libcurl4-openssl-dev exuberant-ctags postgresql pgadmin3 libpq-dev libdbd-pg-perl build-essential cpanminus perl perl-doc software-properties-common python-software-properties python-dev python-pip python3-dev python3-pip
