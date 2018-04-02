cf430ea153c608db39230aa3a8aeaace2118fd52
Adam C. Baker <adam.durandal@gmail.com>
2018-04-02 15:41:36 -0700

    move to urxvt for better config portability

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -8,5 +8,6 @@ link_config zshenv $install_path
 link_config gitignore $install_path
 link_config gitconfig $install_path
 link_config tmux.conf $install_path
-link_config Xdefaults $install_path
+link_config Xresources $install_path
+link_config urxvt $install_path
 cd $old_dir
