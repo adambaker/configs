b287ad72aacfe932a4626fdd5b26988a6def34ef
Adam C. Baker <adam.durandal@gmail.com>
2018-04-08 20:13:11 -0700

    htop

--- install/debian.sh
+++ install/debian.sh
@@ -2,7 +2,7 @@ sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 
 sudo apt-get install zsh gtk-redshift tree neovim tmux silversearcher-ag \
-  rxvt-unicode-256color curl libcurl4-openssl-dev exuberant-ctags \
+  rxvt-unicode-256color curl libcurl4-openssl-dev htop exuberant-ctags \
   postgresql pgadmin3 libpq-dev \
   build-essential software-properties-common \
   libdbd-pg-perl cpanminus perl perl-doc \
